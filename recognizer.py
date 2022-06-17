0b270514a412972e8fa348071613d6e786897c16
Quimzy <bam.boy2020@gmail.com>
2022-06-18 01:36:33 +0300

    computer can gather data

--- recognizer.py
+++ recognizer.py
@@ -1,31 +1,38 @@
 import numpy as np
 import cv2
 
-faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+detector = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+
 cap = cv2.VideoCapture(0)
+
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
-while(True):
-    ret, frame = cap.read()
-    #frame = cv2.flip(frame, - # Flip camera vertically
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-      gray,
-      scaleFactor = 1.2,
-      minNeighbors=5,
-      minSize=(20,50)
-      )
 
-    for (x,y,w,h) in faces:
-      cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-      roi_gray = gray[y:y+h, x:x+w]
-      roi_color = frame[y:y+h, x:x+w] 
-    
-    cv2.imshow('video', frame)
-    
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
-        break
+def gather_dataset():
+  count = 0
+  while count != 30:
+      ret, frame = cap.read()
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cap.release()
-cv2.destroyAllWindows()
+      faces = detector.detectMultiScale(
+        gray,
+        scaleFactor = 1.3,
+        minNeighbors=5,
+        )
+
+      for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        count += 1
+        print(count)
+        cv2.imwrite(f"dataset/img_{count}.jpg", gray[y:y+h,x:x+w])
+      
+        cv2.imshow('video', frame)
+      
+      k = cv2.waitKey(30) & 0xff
+      if k == 27: # press 'ESC' to quit
+          break
+
+  cap.release()
+  cv2.destroyAllWindows()
+
+gather_dataset()
