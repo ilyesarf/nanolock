ee0f7f7be5d5cc9e9a7c17b9952d70d8ac5ef820
Quimzy <bam.boy2020@gmail.com>
2022-08-08 18:25:05 +0300

    store dataset in a subfolder named as the user's md5 hash pt. nanolock

--- recognizer.py
+++ recognizer.py
@@ -4,51 +4,78 @@ import cv2
 import os
 import shutil
 import json
+from hashlib import md5
 
 class Recognizer():
 
   def __init__(self):
-    self.detector = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+
+    self.detector = cv2.CascadeClassifier('nanolock/cascades/haarcascade_frontalface_default.xml')
     self.recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-    if os.path.isfile("names.json") == False:
-      self.names = {"names": ["Unknown"]}
-    
-      self.name = input("What's your name? ")
+    if os.path.isdir("nanolock/dataset/") == False:
+      os.makedirs("nanolock/dataset/")
 
-      self.names["names"].append(self.name)
+  def get_subdirs(self, path): 
+    directories = []
 
-      json.dump(self.names, open("names.json", "w"))
+    for file in os.listdir(path):
+      d = os.path.join(path, file)
+      if os.path.isdir(d):
+        directories.append(d)
+
+    return directories 
+
+  def hash_users(self, users):
+    hashes = [md5(user.encode()).hexdigest() for user in users[1:]]
+
+    return hashes
+
+  def setup(self, username):
+    self.username = username
+    self.dataset_path = f"nanolock/dataset/{md5(self.username.encode()).hexdigest()}"
+
+    self.sub_datasets = self.get_subdirs("nanolock/dataset") 
+
+    if os.path.isfile("usernames.json") == False:
+      self.usernames = {"usernames": ["Unknown"]}
+
+      self.usernames["usernames"].append(self.username)
+
+      json.dump(self.usernames, open("usernames.json", "w"))
 
     else:
-      self.names = json.load(open("names.json", "r"))
+      self.usernames = json.load(open("usernames.json", "r"))
 
-      self.name = input("What's your name? ")
-
-      if self.name not in self.names["names"]:
-        self.names["names"].append(self.name)
-        json.dump(self.names, open("names.json", "w"))
-          
-    if os.path.isdir("dataset") == False:
-      os.makedirs("dataset")
+      if self.username not in self.usernames["usernames"]:
+        self.usernames["usernames"].append(self.username)
+        json.dump(self.usernames, open("usernames.json", "w"))
+    
+    #dataset checks      
+    if os.path.isdir(self.dataset_path) == False:
+      os.makedirs(self.dataset_path)
       self.gather_dataset()
-    elif len(os.listdir("dataset"))%30 != 0:
-      shutil.rmtree("dataset")
-      os.makedirs("dataset")
+
+    elif len(os.listdir(self.dataset_path)) == 0 or len(os.listdir(self.dataset_path))%60 != 0:
+      shutil.rmtree(self.dataset_path)
+      os.makedirs(self.dataset_path)
       self.gather_dataset() 
-    elif (self.names["names"].index(self.name) not in set([imgpath.split(".")[1] for imgpath in os.listdir("dataset")])) == False: #check if there's already data for name
+
+    elif self.dataset_path not in self.sub_datasets: #check if there's already data for user
       self.gather_dataset()
 
-    if os.path.isdir("model") == False:
-      os.makedirs("model")
-      self.train()
-    elif len(os.listdir("model/")) == 0:
-      self.train()
-    elif len(os.listdir("dataset")) > 30 and len(os.listdir("dataset"))%30 == 0: #check if new faces are added to the dataset
-      shutil.rmtree("model")
-      os.makedirs("model")
+    #model checks
+    if os.path.isdir("nanolock/model") == False:
+      os.makedirs("nanolock/model")
       self.train()
 
+    elif len(os.listdir("nanolock/model/")) == 0:
+      self.train()
+
+    elif len(os.listdir(self.dataset_path)) > 60 and len(os.listdir(self.dataset_path))%60 == 0: #check if new faces are added to the dataset
+      shutil.rmtree("nanolock/model")
+      os.makedirs("nanolock/model")
+      self.train()
 
   def gather_dataset(self):
 
@@ -58,42 +85,52 @@ class Recognizer():
     cap.set(4,480) # set Height
     
     count = 0
-    while count != 30:
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while count != 60:
+      ret, frame = cap.read()
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = self.detector.detectMultiScale(
-          gray,
-          scaleFactor = 1.3,
-          minNeighbors=5,
-          )
+      faces = self.detector.detectMultiScale(
+        gray,
+        scaleFactor = 1.3,
+        minNeighbors=5,
+        )
 
-        for (x,y,w,h) in faces:
-          cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-          count += 1
+      for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        count += 1
 
-          cv2.imwrite("dataset/img.%s.%s.jpg" % (self.names["names"].index(self.name), count), gray[y:y+h,x:x+w]) 
-        
-          cv2.imshow('video', frame)
-        
-        k = cv2.waitKey(30) & 0xff
-        if k == 27: # press 'ESC' to quit
-            break
+        cv2.imwrite("nanolock/dataset/%s/img.%s.jpg" % (md5(self.username.encode()).hexdigest(), count), gray[y:y+h,x:x+w]) 
 
     cap.release()
-    cv2.destroyAllWindows()
   
-  def label_img(self):
-    path = "dataset"
-    imgpaths = [os.path.join(path,f) for f in os.listdir(path)]     
+  def label_img(self, usernames=None):
+
+    datasets_path = "nanolock/dataset/"
+    hashed_users = self.hash_users(self.usernames["usernames"])
+
+    imgpaths = []
+    for hashed_user in hashed_users:
+      user_dataset = f"{datasets_path}{hashed_user}"
+      for f in os.listdir(user_dataset):
+        imgpaths.append(os.path.join(user_dataset,f))
+  
     samples=[]
     ids = []
 
     for imgpath in imgpaths:
-        PIL_img = Image.open(imgpath).convert('L') #gray
-        img_numpy = np.array(PIL_img,'uint8') #img to numpy
+      PIL_img = Image.open(imgpath).convert('L') #gray
+      img_numpy = np.array(PIL_img,'uint8') #img to numpy
 
-        id = int(os.path.split(imgpath)[-1].split(".")[1])
+      for hashed_user in hashed_users:
+
+        if hashed_user == imgpath.split("/")[2]:
+          for username in self.usernames["usernames"][1:]:
+            if md5(self.username.encode()).hexdigest() == hashed_user:
+              id = int(self.usernames["usernames"].index(username))
+
+        else:
+          print("Error: %s != %s" % (hashed_user, imgpath.split("/")[2]))
+
         faces = self.detector.detectMultiScale(img_numpy)
 
         for (x,y,w,h) in faces:
@@ -102,21 +139,16 @@ class Recognizer():
 
     return samples,ids
   
-  def train(self):
-    dataset_path = "dataset"
-   
+  def train(self):   
     faces,ids = self.label_img()
 
     self.recognizer.train(faces, np.array(ids))
-    self.recognizer.write('model/model.yml') #save model
+    self.recognizer.write("nanolock/model/model.yml") #save model
 
-  def recognize(self):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
+  def recognize(self, usernames):
     cap = cv2.VideoCapture(0)
 
-    cap.set(3,640) # set Width
-    cap.set(4,480) # set Height
+    font = cv2.FONT_HERSHEY_SIMPLEX
     
     #init id
     id = 0
@@ -124,61 +156,41 @@ class Recognizer():
     minW = 0.1*cap.get(3)
     minH = 0.1*cap.get(4)
 
-    while True:
-        self.recognizer.read("model/model.yml") #load model
+    confidence = 0
+    accept_login = False
 
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        
-        faces = self.detector.detectMultiScale( 
-          gray,
-          scaleFactor = 1.2,
-          minNeighbors = 5,
-          minSize = (int(minW), int(minH)),
-        )
-        
-        for(x,y,w,h) in faces:
-          cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-          id, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
+    chance = 0
 
-          if (confidence < 100):
-            id = self.names["names"][id]
-            confidence = "  {0}%".format(round(100 - confidence))
-          else:
-            id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
-        
-          cv2.putText(
-            frame, 
-            f"Hey {str(id)}", 
-            (x+5,y-5), 
-            font, 
-            1, 
-            (255,255,255), 
-            2
-          )
+    while chance != 5 and accept_login == False:
 
-          cv2.putText(
-            frame, 
-            str(confidence), 
-            (x+5,y+h-5), 
-            font, 
-            1, 
-            (255,255,0), 
-            1
-          ) 
-        
-        cv2.imshow('camera',frame) 
-        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-        if k == 27:
-            break
+      self.recognizer.read("nanolock/model/model.yml") #load model
+
+      ret, frame = cap.read()
+      gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+      
+      faces = self.detector.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+      )
+      
+      for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
+
+        if round(100-confidence) >= 51:
+          accept_login = True
+        else:
+          chance += 1
 
     cap.release()
-    cv2.destroyAllWindows()
+    if accept_login:
+      return usernames[id]
 
-if __name__ == "__main__":
-  recognizer = Recognizer()
-  recognizer.recognize()
-  
-  
-  
+    return accept_login
+
+
+#TODO:  
+#Better facial recognition with PyTorch or TensorFlow
+#Get video stream from JS (client web-browser) to Python (OpenCV)
