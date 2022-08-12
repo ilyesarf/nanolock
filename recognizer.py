8d1490eda00b858b18dadddd9bc5051b43d3add3
Quimzy <bam.boy2020@gmail.com>
2022-08-12 10:27:06 +0300

    dataset augmentation

--- recognizer.py
+++ recognizer.py
@@ -1,9 +1,23 @@
+from genericpath import isdir
 import numpy as np
 from PIL import Image
 import cv2
 import os
 import shutil
 import sys
+
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.models import Model
+
+from tensorflow.keras.optimizers import Adam
+
 
 class Recognizer():
 
@@ -12,6 +26,17 @@ class Recognizer():
       print("cascades folder is not found")
       print("Leaving...")
       sys.exit()
+    
+    if os.path.isdir("nanolock/dataset/") == False:
+      os.makedirs("nanolock/dataset/")
+
+    if os.getenv("SET_USER", None):
+      self.username = input("Set username: ")
+    else:
+      self.username = os.getlogin()
+
+    self.users = {"Unknown": 0}
+    self.size = (244,244)
 
     self.detector = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
     self.recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -22,34 +47,31 @@ class Recognizer():
     self.cap.set(4,480) # set Height
 
     if os.getenv("RESET_RECOG", None):
-      if os.path.isdir("dataset") == True and os.path.isdir("model") == True:
+      if os.path.isdir("dataset") == True: #and os.path.isdir("model") == True:
         shutil.rmtree("dataset")
-        shutil.rmtree("model")
+        #shutil.rmtree("model")
 
     self.setup()
       
   def setup(self):
+    self.dataset_path = f"dataset/{self.username}"
+
     #dataset checks
-    if os.path.isdir("dataset") == False:
-      os.makedirs("dataset")
-      self.gather_dataset()
-    elif len([name for name in os.listdir("dataset") if os.path.isfile(os.path.join("dataset", name))]) < 60:
-      shutil.rmtree("dataset")
-      os.makedirs("dataset")
+    if os.path.isdir(self.dataset_path) == False:
+      os.makedirs(self.dataset_path)
       self.gather_dataset()
 
-    #model checks
-    if os.path.isdir("model") == False:
-      os.makedirs("model")
-      self.train()
-    elif len(os.listdir("dataset/")) == 0:
-      self.train()
+    elif len([name for name in os.listdir(self.dataset_path) if os.path.isfile(os.path.join(self.dataset_path, name))]) < 60:
+      shutil.rmtree(self.dataset_path)
+      os.makedirs(self.dataset_path)
+      self.gather_dataset()
 
+    self.augment_dataset()
 
   def gather_dataset(self):
     count = 0
     while count != 60:
-        ret, frame = self.cap.read()
+        _, frame = self.cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = self.detector.detectMultiScale(
@@ -62,7 +84,7 @@ class Recognizer():
           cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
           count += 1
 
-          cv2.imwrite(f"dataset/img.1.{count}.jpg", gray[y:y+h,x:x+w]) #1 is for the user id
+          cv2.imwrite(f"dataset/{self.username}/{count}.jpg", gray[y:y+h,x:x+w])
         
           cv2.imshow('video', frame)
         
@@ -72,78 +94,27 @@ class Recognizer():
 
     self.cap.release()
     cv2.destroyAllWindows()
-  
-  def label_img(self):
-    path = "dataset"
-    imgpaths = [os.path.join(path,f) for f in os.listdir(path)]     
-    samples=[]
-    ids = []
-
-    for imgpath in imgpaths:
-        PIL_img = Image.open(imgpath).convert('L') #gray
-        img_numpy = np.array(PIL_img,'uint8') #img to numpy
-
-        id = int(os.path.split(imgpath)[-1].split(".")[1])
-        faces = self.detector.detectMultiScale(img_numpy)
-
-        for (x,y,w,h) in faces:
-            samples.append(img_numpy[y:y+h,x:x+w])
-            ids.append(id)
-
-    return samples,ids
-  
-  def train(self):
-    dataset_path = "dataset"
-   
-    faces,ids = self.label_img()
-
-    self.recognizer.train(faces, np.array(ids))
-    self.recognizer.write('model/model.yml') #save model
-
-  def recognize(self):
-    font = cv2.FONT_HERSHEY_SIMPLEX
     
-    #init id
-    id = 0
+  def augment_dataset(self):
 
-    names = ["Unknown", os.getlogin()]
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input)
 
-    minW = 0.1*self.cap.get(3)
-    minH = 0.1*self.cap.get(4)
+    train_generator = train_datagen.flow_from_directory(
+    './dataset',
+    target_size=(224,224),
+    color_mode='rgb',
+    batch_size=32,
+    class_mode='categorical',
+    shuffle=True)
 
-    confidence = 0
-    accept_login = False
+    #print(train_generator.class_indices.values())
 
-    chance = 0
-
-    while chance != 5 and accept_login == False:
-
-      self.recognizer.read("model/model.yml") #load model
-
-      ret, frame = self.cap.read()
-      gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-      
-      faces = self.detector.detectMultiScale( 
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-      )
-      
-      for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-        id, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
-
-        if round(100-confidence) >= 51:
-          accept_login = True
-        else:
-          chance += 1
-
-    self.cap.release()
-    return accept_login
+    return train_datagen, train_generator
 
 recognizer = Recognizer()
-print(recognizer.recognize())
+
+
 
   
   
